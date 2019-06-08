6e9142f8518725c6816eb868ddfa0c8ac549d57f
Richard Angapin <rangapin@gmx.com>
2019-06-09 00:23:09 +0800

    scss improvements

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,7 +7,7 @@ class QuestionsController < ApplicationController
     if @question == 'I am going to work'
       @answer = 'Great!'
     elsif @question.include?('?')
-      @answer = 'Silly question, get dressed and go to work!.'
+      @answer = 'Silly question, get dressed and go to work!'
     else
       @answer = 'I don\'t care, get dressed and go to work!'
     end
