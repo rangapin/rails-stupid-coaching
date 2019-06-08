bc6a203d9a2bb4f0289994542448eded6d9e2ad6
Richard Angapin <rangapin@gmx.com>
2019-06-09 00:34:32 +0800

    added fun background img

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,7 +9,7 @@ class QuestionsController < ApplicationController
     elsif @question.include?('?')
       @answer = 'Silly question, get dressed and go to work!'
     else
-      @answer = 'I don\'t care, get dressed and go to work!'
+      @answer = 'I don\'t care, get ready and go to work!'
     end
   end
 end
